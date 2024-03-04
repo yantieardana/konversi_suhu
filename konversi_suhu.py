2a5eabdeddf01b0806a853b4e597439b94e1d102
yantieardana <yantieardana@gmail.com>
2024-03-04 10:45:51 +0700

    Add files via upload

---
+++ konversi_suhu.py
@@ -0,0 +1,13 @@
+print("KONVERSI SUHU")
+
+celcius = float(input("masukkan celcius: "))
+print ("Suhu dalam celcius adalah = ", celcius, "Celcius")
+
+reamur = (4/5) * celcius
+print ("Suhu dalam Reamur adalah = ", reamur, "Reamur")
+
+kelvin = celcius + 273
+print ("Suhu dalam Kelvin adalah = ", kelvin, "Kelvin")
+
+Fahrenheit = (9/5) * celcius + 32
+print ("Suhu dalam Fahrenheit adalah = ", Fahrenheit, "Fahrenheit")
